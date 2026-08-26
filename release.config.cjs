366f0e0842176a6e3856f352a8710623ed6929e8
Oleg Koval <5700359+oleg-koval@users.noreply.github.com>
2026-08-26 20:58:56 +0200

    fix: keep generated plugin versions in sync

--- release.config.cjs
+++ release.config.cjs
@@ -1,8 +1,15 @@
+const baseConfig = require('semantic-release-npm-github-publish/release.config.js')
+
 /** @type {import('semantic-release').Options} */
 module.exports = {
+  ...baseConfig,
   branches: [
     "main",
     { name: "beta", channel: "beta", prerelease: "beta" },
   ],
-  extends: "semantic-release-npm-github-publish",
+  plugins: [
+    ...baseConfig.plugins.slice(0, 4),
+    "./scripts/semantic-release-build.cjs",
+    ...baseConfig.plugins.slice(4),
+  ],
 };
